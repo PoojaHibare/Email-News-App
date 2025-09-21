21309b54a50369fae8d2b1af6ddebecd2a30cc73
Pooja Hibare <pooja.hibare13@gmail.com>
2025-09-21 16:47:59 +0530

    Add links,Specify language,limit news,add subject,control category

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from pickletools import uint8
-
 import requests
 from SendEmail import send_email
 
+topic="tesla"
+
 api_key="5ff047a2013b4148b21307bb978b39f3"
-url=("https://newsapi.org/v2/everything?q=tesla&"
-     "from=2025-08-20&sortBy=publishedAt&apiKey="
-     "5ff047a2013b4148b21307bb978b39f3")
+url="https://newsapi.org/v2/everything?"\
+     f"q={topic}&"\
+     "sortBy=publishedAt&"\
+     "apiKey=5ff047a2013b4148b21307bb978b39f3&"\
+     "language=en"
 
 #Make request
 request=requests.get(url)
@@ -16,9 +18,12 @@ content=request.json()
 
 #Access the article's titles and description
 body=""
-for article in content["articles"]:
+for article in content["articles"][:20]:
      if article["title"] is not None:
-          body=body+article["title"]+"\n"+article["description"]+2*"\n"
+          body="Subject:Today's News"\
+                +"\n"+body+article["title"]+"\n"\
+                +article["description"]\
+                +"\n"+article["url"]+2*"\n"
 
 body=body.encode("utf-8")
 send_email(message=body)
